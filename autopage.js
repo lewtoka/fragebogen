25e597923be4488e5c34f8422cddf756f3c5b930
yklostermann <yklostermann@hotmail.de>
2025-04-23 15:48:45 +0200

    Update autopage.js

--- autopage.js
+++ autopage.js
@@ -1,17 +1,27 @@
 (function() {
+    let  intervalId = 1000;
+
     function checkForIcon() {
         const button = document.querySelector("#btnNext");
         const icon = document.querySelector('.fa-chevron-right');
+        const form = document.getElementById('fragebogenForm');
+
+        if (form) {
+            return;
+        }
+
         if (icon) {
             console.log('Icon found:', icon);
             clearInterval(intervalId); // Stop checking once the icon is found
             button.click();
-        } else {
-            console.log('Icon not found yet.');
         }
     }
 
-    const intervalId = setInterval(checkForIcon, 1000);
+
+    const frageseite = document.querySelector('.moduleHeader');
+    if ( frageseite) {
+        intervalId = setInterval(checkForIcon, 1000);
+    }
 
 })();
 
